d8ce0c6c76a8b76bb6dc6fc4d67106d85550bd4a
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2018-09-18 09:35:29 +0000

    test

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -40,7 +40,7 @@ foreach($this->vendor as $key => $val){
               <ul class="top_nav_menu">
                 <li class="account">
                   <a href="#">
-                    Akun Saya
+                    My Account
                     <i class="fa fa-angle-down"></i>
                   </a>
                   <ul class="account_selection">
